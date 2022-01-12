03d85ab9d54db4fc2a244031e02f8b7e804d08cb
Yakimov Grigory <73303452+Grigory98@users.noreply.github.com>
2022-01-12 16:18:51 +0400

    Changed function name

--- Unit3/test_items.py
+++ Unit3/test_items.py
@@ -5,7 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_web_site(browser):
+def test_btn_add_to_shopping_cart(browser):
     browser.get(link)
     button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket")))
     assert button, "Кнопка отсутствует"
+    time.sleep(10)
